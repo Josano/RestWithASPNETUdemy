10560bdec220f2a5f96a6b119d7fe58508327162
Josano Soares de Souza <josano.souza@animaeducacao.com.br>
2019-05-15 18:43:14 -0300

    Init Generic Repository Class

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace RestWithASPNETUdemy.Model.Base
+{
+    //Contrato entre os atributos
+    // e a estrutura da Tabela
+    [DataContract]
+    public class BaseEntity
+    {
+
+        public long Id { get; set; }
+
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exist(long? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
